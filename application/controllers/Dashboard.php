6947feedf62a1214e8c252b4990ed0b8f52a8574
Harris <muliadiharris@gmail.com>
2022-02-04 19:17:45 +0700

    masasi

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,7 +1,7 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+
+class Dashboard extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,12 +20,17 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('home');
 		// echo "ini halaman awal";
 	}
-	public function home()
+	public function about()
 	{
-		$this->load->view('home');
+		$this->load->view('about');
+		
+	}
+    public function contact()
+	{
+		$this->load->view('contact');
 		
 	}
 }
